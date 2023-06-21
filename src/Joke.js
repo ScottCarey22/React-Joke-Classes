76896e41cdb9f92f20c81a64ebb97f1891a92378
Scott Carey <scottcarey76@yahoo.com>
2023-06-21 18:12:46 -0400

    app was refactored

--- src/Joke.js
+++ src/Joke.js
@@ -1,18 +1,17 @@
 import React from "react";
 import "./Joke.css";
 
-function Joke({ vote, votes, text, id }) {
-  const upVote = () => vote(id, +1);
-  const downVote = () => vote(id, -1);
+const Joke = ({ vote, votes, text, id }) => {
+  const handleVote = (value) => () => vote(id, value);
 
   return (
     <div className="Joke">
       <div className="Joke-votearea">
-        <button onClick={upVote}>
+        <button onClick={handleVote(1)}>
           <i className="fas fa-thumbs-up" />
         </button>
 
-        <button onClick={downVote}>
+        <button onClick={handleVote(-1)}>
           <i className="fas fa-thumbs-down" />
         </button>
 
